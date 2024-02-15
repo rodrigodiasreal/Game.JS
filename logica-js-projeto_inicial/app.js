0f906bc173ed3a98cdd472a00df9ccc169954ac8
rodrigodiasreal <rodrigod.1988@outlook.com>
2024-02-15 16:26:18 -0300

    Finalização do curso
    Concluído o curso de adivinhar um número em Javascript.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 
 //enquanto o chute não for igual ao número secreto
 while(chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     // Se o "chute" for igual o número secreto.
     if (chute == numeroSecreto){
         break;
